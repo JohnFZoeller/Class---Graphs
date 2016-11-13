55ffcf6ea45e45d19cd88c66b72d47a8c5fbd72c
John Frederick Zoeller <johnzoeller@gmail.com>
2016-11-12 18:05:24 -0800

    fixed some stuff

--- graphm.cpp
+++ graphm.cpp
@@ -17,29 +17,25 @@ GraphM::GraphM()
 
     for(int i = 1; i < MAXNODES; i++)
         for(int j = 1; j < MAXNODES; j++)
-            C[i][j] = INT_MAX;
+            C[i][j] = 102030;
 }
 
 // ------------------------------------buildGraph----------------------------------------------- 
 // Description: calls display help, returns ostream out
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::buildGraph(ifstream& in)   //table type is in charge of all things distance related
-{
+void GraphM::buildGraph(ifstream& in)  {
     int to, from, dumy;
 
     in >> size;
     in.ignore(100, '\n');
 
-    for(int i = 1; i <= size; i ++)
-    {
+    for(int i = 1; i <= size; i ++){
         data[i].setData(in); 
     }
     if(size != 0){
-    for(;;)
-    {
+    for(;;){
         in >> from >> to;
-        if(from == 0) 
-        {
+        if(from == 0) {
             in >> dumy;
             in.clear();
             in.ignore(100, '\n');
@@ -56,27 +52,20 @@ void GraphM::buildGraph(ifstream& in)   //table type is in charge of all things 
 // ------------------------------------findShortestPath----------------------------------------------- 
 // Description: calls display help, returns ostream out
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::findShortestPath()
-{
+void GraphM::findShortestPath(){
     int v = 0;
 
-    for(int source = 1; source <= size; source++)
-    {
+    for(int source = 1; source <= size; source++){
         T[source][source].dist = 0;
 
-        for(int dest = 1; dest <= size; dest++)
-        {
+        for(int dest = 1; dest <= size; dest++){
 
             v = getV(source);
-
             T[v][v].visited = true;
 
-            for(int w = 1; w <= size; w++)        
-            {
-                if((C[v][w] > -1) && (T[w][w].visited == false))
-                {
-                    if((T[source][v].dist + C[v][w]) < T[source][w].dist)
-                    {
+            for(int w = 1; w <= size; w++){
+                if((C[v][w] > -1) && (T[w][w].visited == false)){
+                    if((T[source][v].dist + C[v][w]) < T[source][w].dist){
                         T[source][w].dist =  (T[source][v].dist + C[v][w]);    
                         T[source][w].path = v;    
                     }
@@ -95,17 +84,15 @@ void GraphM::findShortestPath()
 void GraphM::displayAll()
 {
     cout << "Description:      From Node:    To Node:    Djikstra's Path:" << endl;
-    for(int j = 1; j <= size; j++)
-    {
+    for(int j = 1; j <= size; j++){
         cout << data[j] << endl << endl;
 
-        for(int i = 1; i <= size; i++)
-        {
+        for(int i = 1; i <= size; i++){
             if(i == j) continue;
 
             cout << "            " << j << "        " << i << "        ";
 
-            if(T[j][i].dist < INT_MAX) //change to 102030
+            if(T[j][i].dist < 102030) //change to 102030
             {
                 cout << T[j][i].dist << "     ";
                 REC(j, T[j][i].path, T);
@@ -126,18 +113,13 @@ void GraphM::displayAll()
 void GraphM::display(int i, int j)
 {
     cout << i << "       " << j << "     ";
-    if(T[i][j].dist < INT_MAX) //change to 102030
+    if(T[i][j].dist < 102030) //change to 102030 
     {
         cout  << T[i][j].dist << "       ";
-
         REC(i, T[i][j].path, T);
-
         cout << j << endl;
-
         names(i, T[i][j].path, T);
-
         cout << data[j] << endl << endl;
-
     }
     else cout << "----" << endl;
 }
@@ -145,8 +127,7 @@ void GraphM::display(int i, int j)
 // ------------------------------------names----------------------------------------------- 
 // Description: recursive names display function
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::names(int j, int path, TableType T[][MAXNODES])
-{
+void GraphM::names(int j, int path, TableType T[][MAXNODES]){
     if(path == 0)
         cout << "";
     if(path != j)
@@ -157,8 +138,7 @@ void GraphM::names(int j, int path, TableType T[][MAXNODES])
 // ------------------------------------REC----------------------------------------------- 
 // Description: recursive path display function
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::REC(int j, int path, TableType T[][MAXNODES])
-{
+void GraphM::REC(int j, int path, TableType T[][MAXNODES]){
     if(path == 0)
          cout << "";
     if(path != j)
@@ -169,8 +149,7 @@ void GraphM::REC(int j, int path, TableType T[][MAXNODES])
 // ------------------------------------getV----------------------------------------------- 
 // Description: returns the lowest distance 
 // -------------------------------------------------------------------------------------------------------------
-int GraphM::getV(const int vertex) const
-{
+int GraphM::getV(const int vertex) const{
     int low = 0;
 
     for(int i = 1; i <= size; i++)
@@ -189,10 +168,8 @@ int GraphM::getV(const int vertex) const
 //if from, to, or distance is a zero nothing gets inserted. 
 */
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::insertEdge(int from, int to, int d)
-{
-    if(from != 0 && to != 0 && d != 0)
-    {
+void GraphM::insertEdge(int from, int to, int d){
+    if(from != 0 && to != 0 && d != 0){
         if(from < 0)
             from = -from;
         if(to < 0)
@@ -207,116 +184,22 @@ void GraphM::insertEdge(int from, int to, int d)
 // ------------------------------------removeEdge----------------------------------------------- 
 // Description: sets the cost from one node to another to infinity
 // -------------------------------------------------------------------------------------------------------------
-void GraphM::removeEdge(int from, int to)
-{
-    if(from != 0 && to != 0)
-    {
+void GraphM::removeEdge(int from, int to){
+    if(from != 0 && to != 0){
         if(from < 0)
             from = -from;
         if(to < 0)
             to = -to;
 
-        C[from][to] = INT_MAX;
+        C[from][to] = 102030; //possibly change to 102030
     }
 }
 
 // ------------------------------------min----------------------------------------------- 
 // Description: didnt end up using, but returns the smaller of two numbers
 // -------------------------------------------------------------------------------------------------------------
-int GraphM::min(int a, int b)
-{
+int GraphM::min(int a, int b){
     return (a < b) ? a : b;
 }
 
-
-/*void GraphM::findShortestPath()  //ORIGINAL UNMODIFIED
-{
-    int cur = 1;
-    int shortest = 102030;
-    int next = 1;
-    //finding from node 1 only... but to all destinations
-
-    for(int i = 1; i <= size; i++)              //starting at destination 1, working to the last one
-    {
-        cur = 1; 
-        next = 1;
-
-        for(int k = 1; k < MAXNODES; k++)
-            for(int j = 1; j < MAXNODES; j++)
-                T[k][j].visited = false;
-
-            //the thing is that even if there isnt a link at the end of the loop that
-            //doesnt mean there isnt a link, actually there must be a link..
-            //therefore you MIGHT be able to just exit the loop
-        while(next != i)
-        {
-            T[cur][cur].visited = true;
-            T[cur][cur].dist = 0;
-
-            for(int w = 1; w <= size; w++)                                   //working every possLink from current node          
-            {
-                if(!T[cur][w].visited)                                           //target node been visited?
-                {
-                    T[cur][w].dist = min(T[cur][w].dist, (T[cur][cur].dist + C[cur][w]));
-
-                    if(T[cur][w].dist < 102030)
-                        T[cur][w].path = cur;         
-
-                    if(T[cur][w].dist < shortest)
-                    {
-                        shortest = T[cur][w].dist;      
-                        next = w;
-                    }
-                }
-            }//f
-
-            if(T[cur][i].path == 0)   //if there is no path from current = 4  to  i = 5 
-                next = i;
-            else{
-                T[cur][next].visited = true;
-                cur = next;
-                shortest = 102030;
-             }
-
-        }//Ewhile
-    }//Efor
-}
-*/
-
-//second try
-
-/*void GraphM::findShortestPath()
-{
-    -------------resets visited---------------
-    for(int k = 1; k < MAXNODES; k++)
-        for(int j = 1; j < MAXNODES; j++)
-            T[k][j].visited = false;
-    //--------------resets visited----------
-    int v = 0;
-
-    for(int source = 1; source <= size; source++)
-    {
-        T[source][source].dist = 0;
-
-        for(int dest = 1; dest <= size; dest++)
-        {
-            //find v??
-            T[v][v].visited = true;
-
-            for(int w = 1; w <= size; w++)        
-            {
-                if(!T[source][w].visited)
-                {
-                    T[source][w].dist = min(T[source][w].dist, (T[source][v].dist + C[v][w]));    
-
-                    if(T[source][w].dist < T[source][v].dist)
-                    {
-                        T[source][v].dist = T[source][w].dist;  
-                        T[source][v].path = w;    
-                    }
-                }
-            }
-        }
-    }
-}*/
 
